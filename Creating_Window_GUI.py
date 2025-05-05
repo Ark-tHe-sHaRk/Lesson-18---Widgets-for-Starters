9af0aaa1766f06fb90f6444deeb8d2bde3adc9ee
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2025-05-05 16:54:06 +0100

    Creating a GUI Tkinter Window

---
+++ Creating_Window_GUI.py
@@ -0,0 +1,10 @@
+#Importing GUI Tkinter
+from tkinter import *
+
+#Create Window
+window = Tk()
+
+#Set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('400x500')
+window.mainloop()
